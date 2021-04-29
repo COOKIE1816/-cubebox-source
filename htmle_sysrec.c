997583224959539e5a9df695f415c9b73b35ab32
COOKIE <69305087+COOKIE1816@users.noreply.github.com>
2021-04-29 15:50:43 +0200

    Create htmle_sysrec.c

---
+++ htmle_sysrec.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#if defined(__gnu_linux__)
+    char *nameOs = "Linux";
+#elif defined(__WIN32__) || defined(__WIN64__)
+    char *nameOs = " Windows";
+#else
+    char *nameOs = "unknown os";
+#endif
+
+int main() {
+    printf("OS: %s",nameOs);
+    return 0;
+}
