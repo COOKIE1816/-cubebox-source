ec70104a169e0f63b142d842602ea8d34d9abfac
COOKIE <69305087+COOKIE1816@users.noreply.github.com>
2021-04-29 18:58:58 +0200

    Update batcheck.js

--- batcheck.js
+++ batcheck.js
@@ -1,2 +1,10 @@
-var c0 = battery.level;
-sessionStorage.setItem("batLvl",c0);
+function x306() {
+	let c0 = battery.level;
+	let e0 = battery.charging;
+	sessionStorage.setItem("batLvl",c0);
+		if (battery.charging == true){
+			sessionStorage.setItem("batChg","true");
+		} else {sessionStorage.setItem("batChg","false");}
+}
+
+setInterval(x306,500);
